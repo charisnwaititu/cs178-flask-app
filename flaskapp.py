03db314d2a95f219cfcf98bb4dc76c38c522cfef
charisnwaititu <charis.waititu@drake.edu>
2026-03-31 22:33:30 -0500

    debug flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -57,9 +57,8 @@ def display_users():
     rows = execute_query("""
         SELECT Name, Continent, Population
         FROM country
-        LIMIT 20
     """)
-
+    print(rows)
     return render_template('display_users.html', users = rows)
 
 
