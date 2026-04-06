0e519fcf3d196dea10bcff290432d4ed027662c8
charisnwaititu <charis.waititu@drake.edu>
2026-04-05 23:32:45 -0500

    debug

--- flaskapp.py
+++ flaskapp.py
@@ -7,6 +7,19 @@ from flask import render_template
 from flask import Flask, render_template, request, redirect, url_for, flash
 from dbCode import *
 
+import boto3
+from boto3.dynamodb.conditions import Key
+
+REGION = "us-east-1"
+TABLE_NAME = "FavCountries"
+
+def get_table():
+    """Return a reference to the DynamoDB Movies table."""
+    dynamodb = boto3.resource("dynamodb", region_name=REGION)
+    return dynamodb.Table(TABLE_NAME)
+
+
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
                                    # it is required, but you can leave this alone
@@ -30,13 +43,6 @@ def view_continent(continent):
     """, (str(continent),))
         return rows
 
-@app.route("/search-continent", methods=['GET'])
-def continent_form():
-    """
-    Renders an empty form for the user to type a continent name.
-    """
-    return render_template('continents.html', fieldname="Continent")
-
 
 @app.route('/search-continent', methods=['GET', 'POST'])
 def continent_form_post():
@@ -44,15 +50,17 @@ def continent_form_post():
     Reads the continent typed in the form, runs the query, and returns results.
     If no countries are found, flash a warning and redirect back.
     """
-    continent = request.form['continent']
-    rows = view_continent(continent)
+    #CHAT helped with if statement
+    if request.method == ['POST']:
+         continent = request.form['continent']
+         rows = view_continent(continent)
+         if not rows:
+              flash("Continent not found!", "warning")
+              return redirect(url_for('continent_form'))
+         
+         return render_template('display_countries.html', users=rows)
+    return render_template('continents.html', fieldname="Continent")
 
-    #ChatGPT helped me write this code. I wanted a warning to flash when the user types a continent that doesn't exist.
-    if not rows:
-        flash("Continent not found!", "warning")
-        return redirect(url_for('continent_form'))
-
-    return render_template('display_countries.html', users=rows)
 
 
 @app.route('/display-countries')
@@ -80,6 +88,9 @@ def country_capital():
      print('ROW:',rows)
      return render_template('country_capitals.html', users = rows)
   
+
+
+
 
 # these two lines of code should always be the last in the file
 if __name__ == '__main__':
